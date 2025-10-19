aa2f78b640d8dc71463c52cafa391b045006e5f3
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2025-10-19 13:50:07 -0500

    Small Tweak

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,6 +1,6 @@
 export default function PageLayout({ children }) {
   return (
-    <div className="md:bg-stone-100 md:min-h-screen md:py-32 md:px-12">
+    <div className="md:bg-stone-100 md:min-h-screen md:py-32 md:px-12 grid place-items-center">
       {children}
     </div>
   );
